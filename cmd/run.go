5dcba01c3768accd7ac26bc9ffeedb0e7fab9400
Ezequiel Lopes <ezequiel.jn98@outlook.com>
2024-09-03 21:25:25 -0300

    feat: Add CLI and HTTP server functionality for morse code decoding

---
+++ cmd/run.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/ezequieljn/ezequiel-lopes-morse-code/adapters/cli"
+	adaptersHttp "github.com/ezequieljn/ezequiel-lopes-morse-code/adapters/http"
+	"github.com/ezequieljn/ezequiel-lopes-morse-code/application"
+)
+
+func Run() error {
+	mode := flag.String("mode", "cli", "input type")
+	version := flag.String("version", "one", "decode version")
+	decode := flag.String("decode", "", "Texto a ser codificado")
+	space := flag.String("space", "   ", "Espaço entre as palavras")
+	port := flag.String("port", "8080", "Porta do servidor")
+	flag.Parse()
+	decoderService, err := application.NewDecoderFactory(*version, *space)
+	if err != nil {
+		return err
+	}
+	if *mode == "cli" {
+		slog.Info("Running CLI")
+		cliApp := cli.NewCLI(decoderService)
+		return cliApp.Run(*decode)
+	}
+	if *mode == "http" {
+		slog.Info("Running HTTP")
+		httpHandler := adaptersHttp.NewHttpHandler(decoderService)
+		http.HandleFunc("POST /", httpHandler.DecodeMorse)
+		slog.Info(fmt.Sprintf("Servidor HTTP rodando na porta :%s", *port))
+		if err := http.ListenAndServe(fmt.Sprintf(":%s", *port), nil); err != nil {
+			slog.Error(err.Error())
+			return err
+		}
+	}
+	slog.Warn("Invalid mode")
+	return nil
+}
