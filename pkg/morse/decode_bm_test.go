bc74b7d158e208b559e0d3d667141ed1dc52356b
Ezequiel Lopes <ezequiel.jn98@outlook.com>
2024-09-03 21:24:56 -0300

    test: Add benchmark tests for Morse decoders

---
+++ pkg/morse/decode_bm_test.go
@@ -0,0 +1,46 @@
+package morse_test
+
+import (
+	"testing"
+
+	"github.com/ezequieljn/ezequiel-lopes-morse-code/pkg/morse"
+)
+
+func BenchmarkCodeMorse(b *testing.B) {
+	space := "   "
+	textForBenchmark := []string{
+		".... . -.--   .--- ..- -.. .",
+		"       --. .-.. --- -... ---    ",
+		". --.. . --.- ..- .. . .-..",
+		"    .--. .- .-. .- -... . -. ...",
+		".-   --. . -. - .   ... .   ...- .   .--. --- .-.   .- --.- ..- ..",
+		".-   --. . -. - .   ... .   ...- .   .--. --- .-.   .- --.- ..- ..   .-   --. . -. - .   ... .   ...- .   .--. --- .-.   .- --.- ..- ..   .-   --. . -. - .   ... .   ...- .   .--. --- .-.   .- --.- ..- ..   .-   --. . -. - .   ... .   ...- .   .--. --- .-.   .- --.- ..- ..   .-   --. . -. - .   ... .   ...- .   .--. --- .-.   .- --.- ..- ..",
+	}
+
+	b.Run("code morse version: one", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, text := range textForBenchmark {
+				codeMorse := morse.NewMorseTwo(space)
+				codeMorse.Decode(text)
+			}
+		}
+	})
+
+	b.Run("code morse version: two", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, text := range textForBenchmark {
+				codeMorse := morse.NewMorseTwo(space)
+				codeMorse.Decode(text)
+			}
+		}
+	})
+
+	b.Run("code morse version: three", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			for _, text := range textForBenchmark {
+				codeMorse := morse.NewMorseThree(space)
+				codeMorse.Decode(text)
+			}
+		}
+	})
+}
